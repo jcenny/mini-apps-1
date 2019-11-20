11f3aa446c6509a640adbcd7c07c953e1e965228
jcenny <jcenny26@gmail.com>
2019-11-20 11:11:53 -0800

    Complete sending csv report back to client

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,35 @@
+var app = document.getElementById('app');
+//header 
+var header = document.createElement('h1');
+header.innerHTML = 'CSV Report Generator';
+app.append(header);
+//form 
+var form = document.createElement('form');
+var textArea = document.createElement('textarea');
+textArea.setAttribute('type', 'text');
+textArea.setAttribute('class', 'textarea');
+textArea.setAttribute('placeholder', 'JSON data');
+var inputBtn = document.createElement('input');
+inputBtn.setAttribute('type', 'submit');
+inputBtn.setAttribute('class', 'btn')
+form.append(textArea);
+form.append(inputBtn);
+app.append(form);
 
+// form state
+var state = '';
+textArea.setAttribute('value', state);
+// on change, reset state with data
+textArea.addEventListener('change', (event) => {
+  event.preventDefault();
+  state = event.target.value;
+  textArea.setAttribute('value', state);
+})
+
+
+// POST request
+textArea.setAttribute('name', 'data');
+form.setAttribute('method', 'POST');
+form.setAttribute('action', '/upload_JSON');
+console.log(form);
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,61 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
+const app = express();
+const port = 3000;
+
+app.use('/', express.static(path.join(__dirname, './client')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.post('/upload_JSON', (req, res) => {
+  var data = csv(JSON.parse(req.body.data));
+  fs.writeFile(path.join(__dirname, 'serverData', 'csv_report.csv'), data, (err) => {
+    if (err) {
+      res.status(400).send();
+    } else {
+      // res.send(data);
+      res.sendFile(path.join(__dirname, 'serverData', 'csv_report.csv'));
+    }
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
+
+var csv = function (data) {
+  var result = '';
+
+  var labels = function(data) {
+    for (var key in data) {
+      if (key !== 'children') {
+        result = result.concat(key, ',');
+      } else if (key === 'children') {
+        result = result.substring(0, result.length - 1)
+        result = result.concat('\n')
+      }    
+    } 
+  }
+  
+  var getProps = (data) => {
+    for (var key in data) {
+      if (key !== 'children') {
+       result = result.concat(data[key], ',');
+      } else if (key === 'children') {     
+         result = result.substring(0, result.length - 1)
+         result = result.concat('\n')
+      }
+    }
+    data.children.forEach((child) => {
+      getProps(child);  
+    });
+     return result.substring(0, result.length - 1)
+  };
+
+  labels(data);
+  return getProps(data);
+}
 
